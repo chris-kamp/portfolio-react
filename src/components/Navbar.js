0601885bfbfb11f90263e8b34abf2ff28ec06724
Chris Kamp <chriskamp101@gmail.com>
2021-06-04 17:36:44 +1000

    add desktop navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import NavItem from "./NavItem"
+
+const Navbar = () => {
+  return (
+    <nav className="h-20 text-iceBlue font-nunito font-bold text-xl flex justify-between">
+      <div className="flex min-w-max justify-center items-center w-32">
+        <img
+          src="https://picsum.photos/60/60"
+          alt="CK Logo"
+          srcset=""
+          className="rounded-full"
+        />
+      </div>
+      <ul className="flex justify-around tracking-widest flex-auto max-w-2xl">
+        <NavItem text="HOME"/>
+        <NavItem text="ABOUT"/>
+        <NavItem text="PROJECTS"/>
+        <NavItem text="CONTACT"/>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      blueGray: colors.blueGray,
-    }
+    extend: {
+      colors: {
+        blueGray: colors.blueGray,
+        dark: "#020723",
+        iceBlue: "#D8F2F2",
+      },
+      fontFamily: {
+        nunito: ['"Nunito Sans"', "sans-serif"],
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      fontSize: ["hover"],
+      fontWeight: ["hover"],
+      padding: ["hover"],
+    },
   },
   plugins: [],
-}
+};
