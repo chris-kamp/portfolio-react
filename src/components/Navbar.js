dc146d1bbeebf66496c22ad48e429f0c6b89d2c3
Chris Kamp <chriskamp101@gmail.com>
2021-06-05 10:43:28 +1000

    adjust homepage alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import NavItem from "./NavItem"
 const Navbar = () => {
   return (
     <nav className="h-20 text-iceBlue font-nunito font-bold text-xl flex justify-between">
-      <div className="flex min-w-max justify-center items-center w-32">
+      <div className="ml-12 flex min-w-max items-center w-32">
         <img
           src="https://via.placeholder.com/60"
           alt="CK Logo"
