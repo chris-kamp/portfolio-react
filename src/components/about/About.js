2bcbdb5c3b539fa6e6b9248baab54b8ff1370efe
Chris Kamp <chriskamp101@gmail.com>
2021-06-07 21:43:58 +1000

    add bio to about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,7 @@
 import Navbar from "../nav/Navbar"
 import AboutHeading from "./AboutHeading"
+import Card from "./Card"
+import Bio from "./Bio"
 
 const About = () => {
   return (
@@ -8,10 +10,9 @@ const About = () => {
       <main>
         <AboutHeading />
         <div className="flex flex-col items-center">
-          <div className="border-2 border-primary h-96 w-5/6 rounded-lg p-3">
-            <h2 className="text-center text-light text-2xl font-semibold tracking-2xwide ml-2">BIO</h2>
-            <p className="text-light ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat iusto molestias nemo explicabo est. Corporis, ex? Consequuntur exercitationem eveniet culpa possimus asperiores voluptatibus quaerat eligendi inventore, in reiciendis doloremque ipsam.</p>
-          </div>
+          <Card heading="BIO">
+            <Bio />
+          </Card>
         </div>
       </main>
     </div>
