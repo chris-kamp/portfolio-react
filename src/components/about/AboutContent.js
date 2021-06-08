de2050e3b7f4ac26743ac62057e04ba94d3383e9
Chris Kamp <chriskamp101@gmail.com>
2021-06-08 17:40:28 +1000

    implement about page layout up to laptop width

--- src/components/about/AboutContent.js
+++ src/components/about/AboutContent.js
@@ -4,16 +4,20 @@ import TechStack from "./TechStack";
 
 const AboutContent = () => {
   return (
-    <main className="flex flex-col items-center w-full px-3 sm:p-12">
-      <Card heading="BIO">
-        <Bio />
-      </Card>
-      <Card heading="TECH STACK">
-        <TechStack />
-      </Card>
-      <Card heading="HISTORY">
-        <TechStack />
-      </Card>
+    <main className="flex flex-col items-center w-full px-3 lg:flex-row lg:justify-center lg:flex-wrap sm:p-12">
+      <div className="w-full max-w-2xl lg:max-w-none lg:mr-4">
+        <Card heading="BIO">
+          <Bio />
+        </Card>
+      </div>
+      <div className="flex flex-col items-center w-full lg:flex-row">
+        <Card heading="TECH STACK" middle={true}>
+          <TechStack />
+        </Card>
+        <Card heading="HISTORY">
+          <TechStack />
+        </Card>
+      </div>
     </main>
   );
 };
--- src/components/about/Card.js
+++ src/components/about/Card.js
@@ -1,7 +1,7 @@
-const Card = ({heading, ...props}) => {
+const Card = ({heading, middle, ...props}) => {
   return (
-    <article className="w-full p-3 mb-6 border-2 rounded-lg border-primary container">
-      <h2 className="pb-1 pl-2 mb-4 text-xl xs:text-2xl sm:text-3xl font-semibold text-center border-b-2 text-light tracking-2xwide border-primary">{heading}</h2>
+    <article className={`w-full max-w-2xl lg:max-w-none lg:ml-4 p-3 ${middle && "lg:mx-6 2xl:mx-12"} mb-6 border-2 rounded-lg border-primary lg:h-full`}>
+      <h2 className="pb-1 pl-2 mb-4 text-xl font-semibold text-center border-b-2 xs:text-2xl sm:text-3xl text-light tracking-2xwide border-primary">{heading}</h2>
       {props.children}
     </article>
   )
