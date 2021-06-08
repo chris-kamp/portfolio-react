a99d31315b08cb1ace4e55b02ba762e87e4a3a78
Chris Kamp <chriskamp101@gmail.com>
2021-06-09 00:14:45 +1000

    redo about page layout using grid

--- src/components/about/AboutContent.js
+++ src/components/about/AboutContent.js
@@ -5,20 +5,16 @@ import TechStack from "./TechStack";
 
 const AboutContent = () => {
   return (
-    <main className="flex flex-col items-center w-full px-3 lg:flex-row lg:justify-center lg:flex-wrap xl:flex-nowrap sm:px-12 xl:items-stretch">
-      <div className="w-full max-w-2xl lg:max-w-none xl:max-w-2xl xl:w-4/12 lg:mr-4 xl:mr-0">
-        <Card heading="BIO">
-          <Bio />
-        </Card>
-      </div>
-      <div className="flex flex-col items-center w-full lg:flex-row xl:w-8/12">
-        <Card heading="TECH STACK" middle={true}>
-          <TechStack />
-        </Card>
-        <Card heading="HISTORY">
-          <History />
-        </Card>
-      </div>
+    <main className="grid w-full grid-cols-1 gap-4 px-3 md:grid-cols-2 xl:grid-cols-3 xs:gap-6 sm:gap-8 sm:px-12">
+      <Card heading="BIO" first={true}>
+        <Bio />
+      </Card>
+      <Card heading="TECH STACK">
+        <TechStack />
+      </Card>
+      <Card heading="HISTORY">
+        <History />
+      </Card>
     </main>
   );
 };
--- src/components/about/Card.js
+++ src/components/about/Card.js
@@ -1,6 +1,6 @@
-const Card = ({heading, middle, ...props}) => {
+const Card = ({heading, first, ...props}) => {
   return (
-    <article className={`w-full max-w-2xl lg:max-w-none lg:ml-4 p-3 ${middle && "lg:mx-6 xl:mx-6"} xl:mx-0 mb-6 border-2 rounded-lg border-primary lg:h-full xl:mb-0`}>
+    <article className={`mx-auto w-full p-3 border-2 rounded-lg border-primary ${first && " md:col-span-2 xl:col-span-1"}`}>
       <h2 className="pb-1 pl-2 mb-4 text-xl font-semibold text-center border-b-2 xs:text-2xl sm:text-3xl tracking-2xwide border-primary">{heading}</h2>
       {props.children}
     </article>
