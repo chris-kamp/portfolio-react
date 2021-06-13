b50dd9678e5447cc558645bd60171b06c694c030
Chris Kamp <chriskamp101@gmail.com>
2021-06-14 07:21:30 +1000

    make logo a home link

--- src/components/nav/Logo.js
+++ src/components/nav/Logo.js
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import ckLogo from "../../assets/logo-60.png";
 
 const Logo = () => {
   return (
-    <div className="flex items-center ml-3 sm:ml-12 min-w-max">
+    <Link to="/" className="flex items-center ml-3 sm:ml-12 min-w-max">
       <img
         src={ckLogo}
         alt="CK Logo"
       />
-    </div>
+    </Link>
   );
 };
 
