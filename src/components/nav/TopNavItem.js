5234a1c23e27c5d44cc3526ce1ec2b1b9aeb9ce9
Chris Kamp <chriskamp101@gmail.com>
2021-06-14 21:19:03 +1000

    add hover effect to top nav items

--- src/components/nav/TopNavItem.js
+++ src/components/nav/TopNavItem.js
@@ -7,7 +7,7 @@ const TopNavItem = ({ text, destination }) => {
     <li className="flex-shrink-0 h-full px-2 flex-grow-1 block justify-center">
       <NavLink
         to={destination}
-        className={isActive() ? "flex items-center justify-center w-full h-full border-b-4 border-primary pointer-events-none" : "flex items-center justify-center w-full h-full hover:border-b hover:border-light"}
+        className={isActive() ? "flex items-center justify-center w-full h-full border-b-4 border-primary transform scale-105 pointer-events-none" : "flex items-center justify-center w-full h-full hover:border-b hover:border-light transition-transform transform hover:scale-105"}
         exact={true}
       >
         {text}
